01cc8803467ce89fb6b644c444ea8778ea5bead8
jidrovoc <54954960+jidrovoc@users.noreply.github.com>
2021-04-09 12:23:07 -0500

    Update msg.php

--- msg.php
+++ msg.php
@@ -1,42 +1,28 @@
-<?php 
-									if(	isset($_POST["name"])	&&
-										isset($_POST["lastname"])  &&
-										isset($_POST["email"])  &&										
-										isset($_POST["telephone"]) &&
-										isset($_POST['country']) &&
-										isset($_POST['reason']) &&
-										isset($_POST['mensaje'])) {
-										
-										$values = array_map('trim', $_POST);
-										$values = array_map('strip_tags', $_POST);	
-										extract($values);					
-										
-										// send email to administrator (standard)
-										
-										$mail = new PHPMailer;
-										$mail->setFrom('jidrovoc@unemi.edu.ec', 'PESCAGROSA');
-										$mail->addReplyTo('jidrovoc@unemi.edu.ec', 'PESCAGROSA');
-										$mail->addAddress('jidrovoc@unemi.edu.ec', 'PESCAGROSA');
-										$mail->Subject = 'FORMULARIO DE CONTACTO (Posible Cliente)';
-										$mail->CharSet = 'UTF-8';
-										$mail->Body = "Nombre del usuario: $name $lastname | Email del usuario: $email | Teléfono: $telephone | País: $country | Motivo de contacto: $reason | Mensaje del usuario: $mensaje";
-										if (!$mail->send()) {
-										    // echo "Mailer Error: " . $mail->ErrorInfo;
-										} else {
-										    // echo "Message sent!";
-										}
-									
-										// send email to client (mailgun)
-										
-										$mgClient = new Mailgun('key-91aab53a40d85511deb96cac75014ad2');
-										$domain = "pescagrosa.com";
-										
-										try {
-											$result2 = $mgClient->sendMessage($domain, array(
-											    "from"    => "PESCAGROSA <jidrovoc@unemi.edu.ec>",
-											    "to"      => " <$email>",
-											    "subject" => "CONTACTO",
-											    "text"    => "Hola $name $lastname! Gracias por tu interés. Muy pronto uno de nuestros representantes se pondrá en contacto contigo!"
-											));
-											header("Location:./contacto.html");
-											?>
+<?php
+$nombre = $_POST['name'];
+$apellido = $_POST['lastname'];
+$email = $_POST['email'];
+$telefono = $_POST['telephone'];
+$pais = $_POST['country'];
+$razon = $_POST['reason'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+
+
+$para = 'info@pescagrosa.com';
+$asunto = 'FORMULARIO DE CONTACTO (Posible Cliente)';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:Contacto.html");
+?>
