2f5fa2e5131afd7d3743670e22b69c7efaa27b61
jidrovoc <54954960+jidrovoc@users.noreply.github.com>
2021-04-08 20:15:13 -0500

    Add files via upload

---
+++ BananoConvencional_files/botones.js
@@ -0,0 +1,36 @@
+function Origen() {
+	document.getElementById('parrafoPrincipal').innerHTML = "";
+	document.getElementById('ms_2').innerHTML = "";
+	document.getElementById('ms_3').innerHTML = "";
+	document.getElementById('ms_4').innerHTML = "";
+	document.getElementById("ms_1").innerHTML = "El origen del banano en el mundo aparentemente está en el subcontinente hindú en Asia y es trasladado a América<br> conjuntamente con las corrientes migratorias de los comerciantes Europeos en el siglo XVI. De allí varias variedades<br> de musáceas crecen en forma natural y salvaje en<br> La creciente demanda del banano en Europa y Estados Unidos y el impacto de plagas y huracanes en las plantaciones<br> Centroamericanas influyen al gobierno de Galo Plaza Lasso a modernizar la producción y exportación de la fruta.<br>La mano de obra barata, la disponibilidad de tierra agrícola, la política de fomento del gobierno -incluyendo créditos y<br> obras de infraestructura- y la ausencia de plagas y huracanes en el país son las condiciones perfectas para<br> eventualmente posicionar al Ecuador como el primer productor mundial de la fruta.<br>La exportación de banano domina la economía ecuatoriana hasta los años 70, cuando es suplantada por el petróleo.<br>Actualmente Ecuador exporta semanalmente un promedio de 5 millones de cajas, cultivadas en aproximadamente<br> 230 mil hectáreas.";
+
+}
+function Botanica() {
+	document.getElementById('parrafoPrincipal').innerHTML = "";
+	document.getElementById('ms_1').innerHTML = "";
+	document.getElementById('ms_2').innerHTML = "";
+	document.getElementById('ms_3').innerHTML = "";
+	document.getElementById('ms_4').innerHTML = "";
+	document.getElementById("ms_2").innerHTML = "El nombre de plátano, banana, banano, cambur, topocho o guineo agrupa a un gran número de plantas herbáceas<br> del género Musa, tanto híbridos obtenidos horticulturalmente a partir de las especies silvestres del género Musa<br> acuminata y Musa balbisiana como cultivares genéticamente puros de estas especies.<br>Clasificado originalmente por Linnaeus como Musa paradisiaca en 1753, la especie tipo del género Musa, estudios<br> posteriores han llevado a la conclusión de que la compleja taxonomía del género incluye numerosos híbridos, de <br>variada composición genética, y se ha desarrollado un sistema estrictamente sui generis de clasificación para dar<br> cuenta de esta variación.<br>Sin embargo, de acuerdo a las reglas del Código Internacional de Nomenclatura Botánica, el nombre linneano<br> cuenta con prioridad, y sigue siendo usado —tanto en su forma original como en la modificada Musa paradisiaca,<br> que indica que se trata de un híbrido— para designar genéricamente a estas variedades.";
+
+
+}
+function Calidad() {
+	document.getElementById('parrafoPrincipal').innerHTML = "";
+	document.getElementById('ms_1').innerHTML = "";
+	document.getElementById('ms_2').innerHTML = "";
+	document.getElementById('ms_3').innerHTML = "";
+	document.getElementById('ms_4').innerHTML = "";
+	document.getElementById("ms_3").innerHTML = "Una fruta de exportación debe cumplir con varios requisitos al momento de la cosecha y ser aceptados y calificados<br> para exportación.<br>Desde que se siembra la cepa (semilla) hasta que se produce la primera floración dura un tiempo de 24 a 32 semanas<br> dependiendo de la variedad y condiciones ambientales. Una vez que la planta realiza la emisión de la bellota<br> (flor = bellota = racimo), las labores agronómicas son muy importantes y se extienden de 9 a 13 semanas hasta la<br> cosecha.<br>Dependiendo de la época cálida o fría, la edad de corte está determinado por las condiciones de llenado de la fruta,<br> es decir, que ésta haya alcanzado los parámetros de calibración (grosor) y largo del dedo. Esta condición se cumple<br> entre 9 y máximo 13 semanas después de la floración.<br>El transporte de los racimos desde la plantación hasta la planta de proceso (empacadora), se lo realiza por medio del<br> cable vía, en el que viene suspendido el racimo con la ayuda de un dispositivo llamado «garrucha» que tiene un<br> rodamiento especial que le permite viajar en el cable. Esto con el objetivo de evitar daños por manipulación, ya que en<br> estas condiciones la fruta es muy susceptible.";
+
+}
+function Factores() {
+	document.getElementById('parrafoPrincipal').innerHTML = "";
+	document.getElementById('ms_1').innerHTML = "";
+	document.getElementById('ms_2').innerHTML = "";
+	document.getElementById('ms_3').innerHTML = "";
+	document.getElementById('ms_4').innerHTML = "";
+	document.getElementById("ms_4").innerHTML = "Los factores que afectan el crecimiento y productividad de las plantas se clasifican de manera general en factores<br> internos (genéticos) y factores externos (ambientales).<br>Los factores internos están relacionados con la variedad genética utilizada.<br> Los factores externos se relacionan con el clima (luz solar, temperatura y lluvia), agentes bióticos (organismos<br> benéficos y perjudiciales), tipo de suelo y por supuesto la intervención humana que afecta o modifica en cierta<br> medida algunos factores ambientales que determina el manejo agronómico. Por ejemplo, zonas con gran cantidad<br> de brillo solar y temperaturas cálidas (30° C) es potencialmente más productiva que otras zonas con características<br> diferentes.<br>Es muy importante destacar el manejo agronómico, mediante la implementación de variadas y oportunas prácticas<br> agrícolas, contribuye a la obtención de rendimientos altos. Entre las principales se destacan la densidad de siembra,<br> deshije, distribución adecuada de las plantas en el campo, control de malezas, plagas y enfermedades, riego,<br> drenaje, nutrición y prácticas de labores agronómicas oportunas en el cuidado del racimo (enfunde, identificación,<br> deshoje, apuntalamiento), que contribuyen a que la fruta reúna las mejores características de calidad.";
+
+}
